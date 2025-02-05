b472db342bf9d6271257d2566ddc1e77ecaee17a
prasad-bigdp <bigdp585@gmail.com>
2025-02-05 17:29:29 +0530

    update

---
+++ JS/code/datatypes/script.js
@@ -0,0 +1,33 @@
+let a = 25,b=-3.14;
+console.log(typeof a, typeof b);
+a = "prasad"
+console.log(typeof a);
+a = 35;
+console.log(typeof a)
+a = true;
+console.log(typeof a);
+a = 20.1568;
+console.log(typeof a);
+console.log(a.toFixed(2)) //20.157
+console.log(a.toPrecision(3))
+console.log(a);
+console.log(a.toString(16))
+console.log(Math.sqrt(4)) //2
+console.log(Math.cbrt(27)) //3
+console.log(Math.pow(2,3),2**3) //8 8
+console.log(Math.max(5, 4, 1, 2, 4, 8, 7, 12)) //12
+console.log(Math.min(1, 4)) //1
+console.log(Math.ceil(1.21))
+console.log(Math.floor(1.8));
+console.log(Math.round(Math.random() * 1000000))// random value between 0 to 1
+let a1 = Math.ceil(Math.random() * 6);
+let a2 = Math.ceil(Math.random() * 6);
+let a3 = Math.ceil(Math.random() * 6)
+console.log(a1,a2,a3)
+
+
+
+
+
+
+
