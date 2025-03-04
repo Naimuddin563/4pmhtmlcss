b357392c334e9de51de9788c958ef66b83269b72
prasad-bigdp <bigdp585@gmail.com>
2025-03-04 17:26:19 +0530

    update

--- mainProject/cart.js
+++ mainProject/cart.js
@@ -1,8 +1,12 @@
+
+
+
 const cartItems = JSON.parse(localStorage.getItem('products'));
 const mainDiv = document.getElementById('mainDiv');
 displayData(cartItems);
 function displayData (data)
 {
+    mainDiv.textContent=''
     data.forEach((data, i) =>
     {
         const item = document.createElement('div')
@@ -11,12 +15,27 @@ function displayData (data)
 		image.src = data.image
         image.height = 100;
 		const title = document.createElement("h2")
-	   title.textContent = data.title
+        title.textContent = data.title
+        title.classList.add('text-white')
 		const price = document.createElement("p")
 		price.textContent = "Price: ₹" + data.price
 	    const removeCartButton = document.createElement("button")
-		removeCartButton.textContent = "❎"
+        removeCartButton.textContent = "❎"
+        removeCartButton.addEventListener('click',()=>removeCart(i))
         item.append(image, title, price, removeCartButton)
         mainDiv.appendChild(item)
     })
+    findTotal(data);
+}
+function removeCart (ind)
+{
+    cartItems.splice(ind, 1);
+    localStorage.setItem('products', JSON.stringify(cartItems));
+    displayData(cartItems)
+    findTotal(cartItems)
+}
+function findTotal (arr)
+{
+    const t = arr.reduce((prev, curr) => prev + curr.price, 0)
+    document.getElementById('total').textContent = "Total: ₹" + t;
 }
--- mainProject/script.js
+++ mainProject/script.js
@@ -1,3 +1,4 @@
+AOS.init()
 const swiper = new Swiper(".swiper", {
 	// Optional parameters
 	direction: "vertical",
@@ -12,54 +13,48 @@ const swiper = new Swiper(".swiper", {
 	},
 })
 
-async function fetchData ()
-{
-    try
-    {
-        const res = await fetch("https://fakestoreapi.com/products")
-        if (res.ok)
-        {
-            const data = await res.json();
-            const electronicProducts = data.filter((v) => v.category == 'electronics')
-            displayData("electronicsData", electronicProducts)
-            const mensProducts = data.filter((v) => v.category == "men's clothing",)
-            displayData('mensData', mensProducts)
-            const womensProducts = data.filter((v) => v.category == "women's clothing",)
-            displayData("womensData", womensProducts)
-            const jewelleryProducts = data.filter(
-							(v) => v.category == "jewelery",
-						)
-						displayData("jewelleryData", jewelleryProducts)
-        }
-    }   
-    catch (err)
-    {
-        console.log(err)
-    }
+async function fetchData() {
+	try {
+		const res = await fetch("https://fakestoreapi.com/products")
+		if (res.ok) {
+			const data = await res.json()
+			const electronicProducts = data.filter((v) => v.category == "electronics")
+			displayData("electronicsData", electronicProducts)
+			const mensProducts = data.filter((v) => v.category == "men's clothing")
+			displayData("mensData", mensProducts)
+			const womensProducts = data.filter(
+				(v) => v.category == "women's clothing",
+			)
+			displayData("womensData", womensProducts)
+			const jewelleryProducts = data.filter((v) => v.category == "jewelery")
+			displayData("jewelleryData", jewelleryProducts)
+		}
+	} catch (err) {
+		console.log(err)
+	}
 }
-fetchData();
+fetchData()
 
-function displayData (id, products)
-{
-    const mainDiv = document.getElementById(id);
-    products.forEach((p, i) =>
-    {
-        const productDiv = document.createElement('div')
-        productDiv.classList.add('card')
-        const productImage = document.createElement('img');
-        productImage.classList.add('card-img-top')
-        productImage.height = 200;
-        productImage.src = p.image; productImage.alt = p.title;
-        const productTitle = document.createElement('h2');
-        productTitle.classList.add("card-title");
-        productTitle.textContent = p.title;
-        const productPrice = document.createElement('p');
-        productPrice.classList.add("card-text")
-        productPrice.textContent = "Price: ₹" + p.price;
-        const addCartButton = document.createElement('button');
-        addCartButton.classList.add(["btn", "btn-primary"])
-        addCartButton.text="Add Cart"
-        productDiv.append(productImage, productTitle, productPrice, addCartButton)
-        mainDiv.appendChild(productDiv)
-    })
+function displayData(id, products) {
+	const mainDiv = document.getElementById(id)
+	products.forEach((p, i) => {
+		const productDiv = document.createElement("div")
+		productDiv.classList.add("card")
+		const productImage = document.createElement("img")
+		productImage.classList.add("card-img-top")
+		productImage.height = 200
+		productImage.src = p.image
+		productImage.alt = p.title
+		const productTitle = document.createElement("h2")
+		productTitle.classList.add("card-title")
+		productTitle.textContent = p.title
+		const productPrice = document.createElement("p")
+		productPrice.classList.add("card-text")
+		productPrice.textContent = "Price: ₹" + p.price
+		const addCartButton = document.createElement("button")
+		addCartButton.classList.add(["btn", "btn-primary"])
+		addCartButton.text = "Add Cart"
+		productDiv.append(productImage, productTitle, productPrice, addCartButton)
+		mainDiv.appendChild(productDiv)
+	})
 }
